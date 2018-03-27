175ac1053a33fcbdc9bfcc6798c18a2cd5b9805f
Dmidma <d.oussema.d@gmail.com>
2018-03-27 09:05:04 +0100

    Setting up user entity

---
+++ api/src/Controller/UserController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Encoder\EncoderFactory;
+        
+
+class UserController extends Controller
+{
+    /**
+     * @Route(
+     *     name="api_users_post",
+     *     path="/users",
+     *     methods={"POST"},
+     *     defaults={
+     *         "_api_resource_class"=User::class,
+     *         "_api_collection_operation_name"="post"
+     *     }
+     * )
+     */
+    public function postAction(User $data, UserPasswordEncoderInterface $encoder): User
+    {
+        return $this->encodePassword($data, $encoder);
+    }
+
+    /**
+     * @Route(
+     *     name="api_users_put",
+     *     path="/users/{id}",
+     *     requirements={"id"="\d+"},
+     *     methods={"PUT"},
+     *     defaults={
+     *         "_api_resource_class"=User::class,
+     *         "_api_item_operation_name"="put"
+     *     }
+     * )
+     */
+    public function putAction(User $data, UserPasswordEncoderInterface $encoder): User
+    {
+        return $this->encodePassword($data, $encoder);
+    }
+
+
+    /**
+     * @Route(
+     *     name="check_user",
+     *     path="/login"
+     * )
+     */
+    public function checkAction(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $content = $request->getContent();
+        $parametersAsArray = json_decode($content, true);
+
+        $userRepo = $this->getDoctrine()
+            ->getRepository(User::class);
+
+        $user = $userRepo->loadUserByUsername($parametersAsArray["username"]);
+
+        if (is_null($user)) {
+            return new JsonResponse([
+                "error" => "Wrong Credentials",
+            ]);
+        }
+
+        $isValid = $encoder->isPasswordValid($user, $parametersAsArray["password"], $user->getSalt());
+
+        if (!$isValid) {
+            return new JsonResponse([
+                "error" => "Wrong Credentials",
+            ]);
+        }
+
+        $token = base64_encode(random_bytes(random_int(10, 50)));
+        return new JsonResponse([
+            "username" => $user->getUsername(),
+            "id" => $user->getId(),
+            "email" => $user->getEmail(),
+            "token" => $token 
+        ]);
+    }
+
+    protected function encodePassword(User $data, UserPasswordEncoderInterface $encoder): User
+    {
+        $encoded = $encoder->encodePassword($data, $data->getPassword());
+        $data->setPassword($encoded);
+
+        return $data;
+    }
+}
---
+++ api/src/Entity/User.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+
+/**
+ * @ORM\Table(name="app_users")
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ *
+ * @ApiResource(
+ *  itemOperations={
+ *      "get",
+ *      "delete",
+ *      "put"={"route_name"="api_users_put"}
+ *  }, collectionOperations={
+ *      "get",
+ *      "post"={"route_name"="api_users_post"}
+ *  }
+ * )
+ * @ApiFilter(SearchFilter::class, properties={"username": "exact", "email": "partial"})
+ */
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=25, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActive;
+
+
+    public function __construct()
+    {
+        $this->isActive = true;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        
+
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+
+    }
+    
+    public function serialize()
+    {
+        return serialize(array(
+            $this-id,
+            $this->username,
+            $this->password
+        ));
+    }
+
+
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password
+        ) = unserialize($serialized);
+    }
+}
